e3dd448e22a04a7333e0128f2e37f4710e36a0d3
huzaifa9811 <123245200+huzaifa9811@users.noreply.github.com>
2025-03-08 16:56:00 +0530

    Update SecurityConfiguration.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/configuration/SecurityConfiguration.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/configuration/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.jtspringproject.JtSpringProject.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration; 
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
