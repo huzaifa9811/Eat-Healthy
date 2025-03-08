c7918ea69c77e81218c6a8a390cc007cf71fb993
huzaifa9811 <123245200+huzaifa9811@users.noreply.github.com>
2025-03-08 17:07:37 +0530

    Update JtSpringProjectApplication.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -1,5 +1,5 @@
 package com.jtspringproject.JtSpringProject;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
